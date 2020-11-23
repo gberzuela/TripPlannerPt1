11b2ea4170f2eb4267e8fd22a07107d9cddbc8f0
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-11-23 09:08:22 -0800

    update: stcuk on mapbox api key

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+import mapboxgl from 'mapbox-gl';
+
+console.log('Webpack worked!');
+
+mapboxgl.accessToken =
+	'pk.eyJ1IjoiZ3VtamEiLCJhIjoiY2todXNnOTViMHdwZjJ6b2x2aHduOWQ2ayJ9.n24_sLNch5m_hNjQRxfQ9A';
+
+const map = new mapboxgl.Map({
+	container: 'map',
+	center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+	zoom: 12, // starting zoom
+	style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
